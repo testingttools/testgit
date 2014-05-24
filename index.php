b6ab03542f74fa3f4098970635e92032b406985b
Allan Hyde <allan.hyde@tektools.com.au>
2014-05-24 18:21:27 +1000

    Initial Commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+//got test project index page
+include_once ("includes/header.php");
+?>
+<h1>Home Page Header</h1>
+<p>Some Home page text goes here</p>
+<?php
+include_once ("includes/footer.php");
